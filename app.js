eb841a62ddb25741d5c4b85475517590125d7e34
Harris Ho <>
2023-01-03 15:54:12 -0800

    update server app.js

--- app.js
+++ app.js
@@ -24,8 +24,11 @@ app.use(require("./routes/auth"));
 app.use(require("./routes/post"));
 app.use(require("./routes/user"));
 
-if (process.env.NODE_ENV == "Production") {
+if (process.env.NODE_ENV === "production") {
+  // Exprees will serve up production assets
   app.use(express.static("client/build"));
+
+  // Express serve up index.html file if it doesn't recognize route
   const path = require("path");
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
